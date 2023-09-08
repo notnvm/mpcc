56a16009f38afad4ad5a4a01225c9b2e265183c0
notnvm <auraz616@mail.ru>
2023-09-08 13:16:10 +0300

    add new bench for unequal sizes

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -1,9 +1,9 @@
 #include <benchmark/benchmark.h>
 #include "lib.h"
 
-std::vector<int> mpcc_bench(size_t size, int min, int max) {
-    std::vector<int> a{ generator::create_random_seq(size, min, max) };
-    std::vector<int> b{ generator::create_convex_seq(size, min, max) };
+std::vector<int> mpcc_bench(size_t size_a, size_t size_b, int min, int max) {
+    std::vector<int> a{ generator::create_random_seq(size_a, min, max) };
+    std::vector<int> b{ generator::create_convex_seq(size_b, min, max) };
 
     std::vector<int> c = smawk::mpcc(a, b);
 
@@ -22,11 +22,20 @@ std::vector<int> mpcc_allzero_bench(size_t size) {
 void mpcc_eqs(benchmark::State& state) {
     while (state.KeepRunning()) {
         benchmark::DoNotOptimize(
-            mpcc_bench(state.range(0), 1, 100)
+            mpcc_bench(state.range(0), state.range(0), 1, 100)
         );
     }
 }
 BENCHMARK(mpcc_eqs)->Name("< min,+ > - convex convolution: equal-size")->RangeMultiplier(10)->Range(10, 1'000'000)->Unit(benchmark::kMillisecond);
+
+void mpcc_uneqs(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(
+            mpcc_bench(state.range(0), 10, 1, 100)
+        );
+    }
+}
+BENCHMARK(mpcc_uneqs)->Name("< min,+ > - convex convolution: unequal-size(M - const)")->RangeMultiplier(10)->Range(10, 1'000'000)->Unit(benchmark::kMillisecond);
 
 void mpcc_all_zero(benchmark::State& state) {
     while (state.KeepRunning()) {
